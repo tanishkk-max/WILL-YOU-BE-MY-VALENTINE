fdb5984ab6978de125df50d4ec457f7234b07c78
tanishkk-max <tanishktiwari2008@gmail.com>
2025-02-06 14:46:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ function selectOption(option) {
             displayCatHeart(); // Display the cat-heart.gif
         });
     } else if (option === 'no') {
-        // Change text on the "No" button to "You sure?"
-        document.getElementById('no-button').innerText = 'You sure?'; 
+        // Change text on the "No" button to "pakka ishuu?"
+        document.getElementById('no-button').innerText = 'pakka ishuu?'; 
         // Increase font size of "Yes" button
         var yesButton = document.getElementById('yes-button');
         var currentFontSize = window.getComputedStyle(yesButton).getPropertyValue('font-size');
